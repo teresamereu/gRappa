398bdc926bd2bca6b078200670b67225e26569d9
Andrea Bianco <andrea.bianco00.ab@gmail.com>
2026-04-16 13:32:53 +0200

    legendary grappa_sborra function

---
+++ R/grappa_sborra.R
@@ -0,0 +1,16 @@
+#' Print a motivational gRappa message
+#'
+#' This function prints a very strong message in the console.
+#'
+#' @return No return value, prints a message
+#' @export
+
+grappa_sborra <- function() {
+
+  cat("\n")
+  cat("=========================================\n")
+  cat("SBORRA, SBORRA NELLA GRAPPA, GRAPPA NELLA SBORRA\n")
+  cat("=========================================\n")
+  cat("\n")
+
+}
